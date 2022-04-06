f73b80742a7785d329b856042e32fb68108079de
Shohei Nagata <shohei.star@gmail.com>
2022-04-06 15:53:10 +0900

    update score.py

---
+++ score.py
@@ -0,0 +1,13 @@
+import joblib
+import os
+
+def init():
+    global model
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
+
+    model = joblib.load(model_path)
+
+def run(mini_batch):
+    print(f"run method start: {__file__}, run({mini_batch})")
+    result = model.predict(mini_batch)
+    return result.tolist()
